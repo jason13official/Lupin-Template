c3994a35a906ce56d5073720c5ea5054b93464fd
jason13official <jason13official@gmail.com>
2024-10-18 18:11:36 -0400

    fix: simplify mixins before debug removal

--- common/src/main/java/com/cursee/examplemod/mixin/MinecraftMixin.java
+++ common/src/main/java/com/cursee/examplemod/mixin/MinecraftMixin.java
@@ -15,9 +15,8 @@ public class MinecraftMixin {
     @Inject(at = @At("TAIL"), method = "<init>")
     private void init(CallbackInfo info) {
 
-        if (ExampleMod.debugCommon) {
-            Constants.LOG.info("This line is printed by a mixin loaded in a {} instance!", "Common");
-            Constants.LOG.info("MC Version: {}", Minecraft.getInstance().getVersionType());
+        if (Services.PLATFORM.isDevelopmentEnvironment()) {
+            Constants.LOG.info("An instance of Minecraft was created in a development environment!");
         }
     }
 }
