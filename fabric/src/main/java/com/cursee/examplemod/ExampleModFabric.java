4c973273855b96fe0d70e93798571992028b7110
jason13official <jason13official@gmail.com>
2024-10-18 19:20:13 -0400

    remove unused import

--- fabric/src/main/java/com/cursee/examplemod/ExampleModFabric.java
+++ fabric/src/main/java/com/cursee/examplemod/ExampleModFabric.java
@@ -1,6 +1,5 @@
 package com.cursee.examplemod;
 
-import com.cursee.monolib.core.MonoLibConfiguration;
 import com.cursee.monolib.core.sailing.Sailing;
 import net.fabricmc.api.ModInitializer;
 
@@ -8,7 +7,6 @@ public class ExampleModFabric implements ModInitializer {
     
     @Override
     public void onInitialize() {
-
         ExampleMod.init();
         Sailing.register(Constants.MOD_NAME, Constants.MOD_ID, Constants.MOD_VERSION, Constants.MC_VERSION_RAW, Constants.PUBLISHER_AUTHOR, Constants.PRIMARY_CURSEFORGE_MODRINTH);
     }
